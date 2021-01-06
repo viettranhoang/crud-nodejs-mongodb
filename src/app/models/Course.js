6f3b4cf0944ca7a46a5b658641afca20a75001de
vit <anhxtanhwp@gmail.com>
2021-01-06 17:14:13 +0700

    delete courses

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const slug = require('mongoose-slug-generator');
-mongoose.plugin(slug);
+const mongooseDelete = require('mongoose-delete');
 
 const Course = new Schema(
     {
@@ -10,11 +10,17 @@ const Course = new Schema(
         image: { type: String, maxlength: 255 },
         videoId: { type: String, required: true },
         level: { type: String },
-        slug: { type: String, slug: 'name' },
+        slug: { type: String, slug: 'name', unique: true },
     },
     {
         timestamps: true,
     },
 );
+
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: 'all',
+});
 
 module.exports = mongoose.model('Course', Course);
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
+router.get('/:id/edit', courseController.edit);
+router.put('/:id', courseController.update);
+router.delete('/:id', courseController.destroy);
+router.delete('/:id/force', courseController.forceDestroy);
+router.patch('/:id/restore', courseController.restore);
 router.post('/store', courseController.store);
 router.get('/:slug', courseController.show);
 
